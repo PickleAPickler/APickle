c3d44260fab5215bf1b46431dee312758988ccee
Alan <a.nicholson2358@gmail.com>
2025-12-13 08:44:45 +0000

    adding everything

---
+++ TTT_Testing/test_learning/name_function.py
@@ -0,0 +1,17 @@
+
+'''Testing unittest - Python Crash Course ch.11 '''
+
+# This is name_function.py  ... we could save it as a separate file,
+# but how to we save it as a file within this script? 
+
+def get_formatted_name(first, last):
+
+    full_name = f'{first}, {last}'
+    return full_name.title()
+
+get_formatted_name('Allen', 'Poe')
+
+
+
+
+
